1fda90ff9a902f436e5b807da366eb01362e6d3e
Tianyi Lin <tianyi.lin.tl@gmail.com>
2021-01-16 22:47:15 -0800

    ui/ux but a lot of errors

--- app/src/main/java/com/example/dream_sbhacks/ChooseLoginorRegistrationActivity.java
+++ app/src/main/java/com/example/dream_sbhacks/ChooseLoginorRegistrationActivity.java
@@ -1,19 +1,26 @@
 package com.example.dream_sbhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ChooseLoginorRegistrationActivity extends AppCompatActivity {
 
     private Button mLogin, mRegister;
+    ConstraintLayout constraintLayout;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_loginor_registration);
+
+        constraintLayout = findViewById(R.id.container);
+        textView = findViewById(R.id.textView);
 
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
