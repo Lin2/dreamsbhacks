c76c5a36a0d54a6a6e612883abbceb3b371c531f
Tianyi Lin <tianyi.lin.tl@gmail.com>
2021-01-16 23:41:35 -0800

    ??

--- app/src/main/java/com/example/dream_sbhacks/ChooseLoginorRegistrationActivity.java
+++ app/src/main/java/com/example/dream_sbhacks/ChooseLoginorRegistrationActivity.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ public class ChooseLoginorRegistrationActivity extends AppCompatActivity {
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getActionBar().hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_loginor_registration);
 
