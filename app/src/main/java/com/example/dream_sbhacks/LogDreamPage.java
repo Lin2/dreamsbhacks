0ee9d33190c5cffa7762d4c98407ae5d0cd09e73
Amy He <39035908+Amyh11325@users.noreply.github.com>
2021-01-17 07:10:24 -0800

    Add files via upload

--- app/src/main/java/com/example/dream_sbhacks/LogDreamPage.java
+++ app/src/main/java/com/example/dream_sbhacks/LogDreamPage.java
@@ -5,9 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
@@ -27,6 +33,10 @@ public class LogDreamPage extends AppCompatActivity {
     private ListView listView;
     private Button button;
     private Button button2;
+    private FirebaseAuth mAuth;
+    private int entryCount;
+    String userId;
+    String lastString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,7 @@ public class LogDreamPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LogDreamPage.this, MainActivity.class);
+                intent.putExtra("LastString",lastString );
                 startActivity(intent);
                 finish();
             }
@@ -59,6 +70,26 @@ public class LogDreamPage extends AppCompatActivity {
         listView.setAdapter(itemsAdapter);
         setUpListViewListener();
 
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getCurrentUser().getUid();
+        entryCount = 0;
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                while (dataSnapshot.child("entry" + (entryCount+1)).exists()) {
+                    itemsAdapter.add(dataSnapshot.child("entry" + (entryCount+1)).getValue().toString());
+                    lastString = dataSnapshot.child("entry" + (entryCount+1)).getValue().toString();
+                    entryCount++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 
     private void setUpListViewListener() {
@@ -84,7 +115,13 @@ public class LogDreamPage extends AppCompatActivity {
         //check user has typed something, not empty string
         if(!(itemText.equals(""))) {
             itemsAdapter.add(itemText);
+            lastString = itemText;
+
+            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId).child("entry"+(entryCount + 1));
+            currentUserDb.setValue(itemText);
+            entryCount++;
             input.setText(""); //set back to empty
+
         } else{
             Toast.makeText(getApplicationContext(), "No dreams, head empty.", Toast.LENGTH_LONG).show();
         }
--- app/src/main/java/com/example/dream_sbhacks/MainActivity.java
+++ app/src/main/java/com/example/dream_sbhacks/MainActivity.java
@@ -27,6 +27,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,12 +68,12 @@ public class MainActivity extends AppCompatActivity {
                 //Do something on the left!
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
-                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -102,9 +106,42 @@ public class MainActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 if (snapshot.exists())
                 {
+                    int userEntryCount = 0;
                     if (!snapshot.getKey().equals(userId)){
-                        al.add(snapshot.child("name").getValue().toString());
-                        arrayAdapter.notifyDataSetChanged();}
+                       // al.add(snapshot.child("name").getValue().toString());
+                        arrayAdapter.notifyDataSetChanged();
+                        int entryCount = 0;
+                        while(snapshot.child("entry" + (entryCount + 1)).exists()) {
+                            al.add(snapshot.child("entry" + (entryCount + 1)).getValue().toString());
+                            entryCount++;
+                        }
+                    } else {
+                       /* int entryCount = 0;
+                        while(snapshot.child("entry" + (entryCount + 1)).exists()) {
+                            entryCount++;
+                        }
+                        userEntryCount = entryCount;
+                        System.out.println(entryCount);*/
+                    }
+
+                    final String curDream;
+
+                    Bundle extras = getIntent().getExtras();
+                    if (extras != null) {
+                        curDream = extras.getString("LastString");
+                    } else {
+                        curDream = null;
+                    }
+
+                    arrayAdapter.notifyDataSetChanged();
+                    Collections.sort(al, new Comparator<String>() {
+                        @Override
+                        public int compare(String o1, String o2) {
+                            System.out.println(curDream + ": " + o1 + ": " + o2 + ": " + (stringComp(o1, curDream) - stringComp(o2, curDream)));
+                            return (int)(10 * (stringComp(o2, curDream) - stringComp(o1, curDream)));
+                        }
+                    });
+                    arrayAdapter.notifyDataSetChanged();
                     }
                 }
 
@@ -135,5 +172,61 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent (MainActivity.this, ChooseLoginorRegistrationActivity.class);
         finish();
         return;
+    }
+
+    public static double stringComp(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return 0;
+        }
+        s1 += " ";
+        s2 += " ";
+        Set<String> set = new HashSet<>();
+        int count1 = 0;
+        int count2 = 0;
+        int totalScore = 0;
+        int start = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (Character.toUpperCase(s1.charAt(i)) < 'A' || Character.toUpperCase(s1.charAt(i)) > 'Z') {
+                if (i != start) {
+                    String w = s1.substring(start, i);
+                    w = w.toUpperCase();
+                    if (w.endsWith("ING")) {
+                        w = w.substring(0, w.length() - 3);
+                    } else if (w.endsWith("ED")) {
+                        w = w.substring(0, w.length() - 2);
+                    } else if (w.endsWith("S")) {
+                        w = w.substring(0, w.length() - 1);
+                    }
+                    count1++;
+                    set.add(w);
+                }
+                start = i + 1;
+            }
+        }
+
+        start = 0;
+        for (int i = 0; i < s2.length(); i++) {
+            if (Character.toUpperCase(s2.charAt(i)) < 'A' || Character.toUpperCase(s2.charAt(i)) > 'Z') {
+                if (i != start) {
+                    String w = s2.substring(start, i);
+                    w = w.toUpperCase();
+                    if (w.endsWith("ING")) {
+                        w = w.substring(0, w.length() - 3);
+                    } else if (w.endsWith("ED")) {
+                        w = w.substring(0, w.length() - 2);
+                    } else if (w.endsWith("S")) {
+                        w = w.substring(0, w.length() - 1);
+                    }
+                    count2++;
+
+                    if(set.contains(w)) {
+                        totalScore++;
+                    }
+                }
+                start = i + 1;
+            }
+        }
+
+        return ((double)totalScore)/count2;
     }
 }
